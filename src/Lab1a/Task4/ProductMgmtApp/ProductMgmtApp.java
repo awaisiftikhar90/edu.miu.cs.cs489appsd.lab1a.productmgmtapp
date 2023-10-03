930f811e0d0be7ef0101481dad0af93e507121c7
Awais iftikhar <awaisiftikhar90@gmail.com>
2023-10-03 01:45:44 -0500

    model

--- src/Lab1a/Task4/ProductMgmtApp/ProductMgmtApp.java
+++ src/Lab1a/Task4/ProductMgmtApp/ProductMgmtApp.java
@@ -1,8 +1,84 @@
 package Lab1a.Task4.ProductMgmtApp;
 
+import Lab1a.Task4.model.Product;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+
 public class ProductMgmtApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello CS489");
+        // Creating an array of Products with company data
+        Product[] products = {
+                new Product(3128874119L, "Banana", new Date(2023 - 1900, 0, 24), 124, 0.55),
+                new Product(2927458265L, "Apple", new Date(2022 - 1900, 11, 9), 18, 1.09),
+                new Product(9189927460L, "Carrot", new Date(2023 - 1900, 2, 31), 89, 2.99)
+                // Add more products as needed
+        };
+        // Sorting products by name in ascending order
+        Arrays.sort(products, Comparator.comparing(Product::getName));
+
+        // Print products in different formats
+        printProducts(products, Format.JSON);
+        printProducts(products, Format.XML);
+        printProducts(products, Format.CSV);
     }
+
+    private static void printProducts(Product[] products, Format format) {
+        switch (format) {
+            case JSON:
+                printJSON(products);
+                break;
+            case XML:
+                printXML(products);
+                break;
+            case CSV:
+                printCSV(products);
+                break;
+            default:
+                System.out.println("Unsupported format");
+        }
+    }
+    private static void printJSON(Product[] products) {
+        JSONObject json = new JSONObject(str);
+        String xml = XML.toString(json);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(products);
+            System.out.println("JSON Format:");
+            System.out.println(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void printXML(Product[] products) {
+        XmlMapper xmlMapper = new XmlMapper();
+        try {
+            String xml = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(products);
+            System.out.println("XML Format:");
+            System.out.println(xml);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void printCSV(Product[] products) {
+        System.out.println("CSV Format:");
+        System.out.println("productId,name,dateSupplied,quantityInStock,unitPrice");
+        for (Product product : products) {
+            System.out.println(product.getProductId() + "," +
+                    product.getName() + "," +
+                    new SimpleDateFormat("yyyy-MM-dd").format(product.getDateSupplied()) + "," +
+                    product.getQuantityInStock() + "," +
+                    product.getUnitPrice());
+        }
+    }
+    // Enum to represent different output formats
+    private enum Format {
+        JSON,
+        XML,
+        CSV
+    }
+
 }
---
+++ src/Lab1a/Task4/model/Product.java
@@ -0,0 +1,60 @@
+package Lab1a.Task4.model;
+
+import java.util.Date;
+
+public class Product {
+
+    private long productId;
+    private String name;
+    private Date dateSupplied;
+    private int quantityInStock;
+    private double unitPrice;
+
+    public Product(long productId, String name, Date dateSupplied, int quantityInStock, double unitPrice) {
+        this.productId = productId;
+        this.name = name;
+        this.dateSupplied = dateSupplied;
+        this.quantityInStock = quantityInStock;
+        this.unitPrice = unitPrice;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDateSupplied() {
+        return dateSupplied;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateSupplied(Date dateSupplied) {
+        this.dateSupplied = dateSupplied;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}
